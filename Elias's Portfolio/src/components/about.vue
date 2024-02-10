e3a25a4496650a9dc18f20879b97604c027f3515
Elias Josué HAJJAR LLAUQUEN <elias-josue.hajjar-llauquen@epitech.eu>
2024-02-11 00:06:28 +0100

    [ADD][UPDATE] content finished about my personnal projects & some minor bugs correction

--- Elias's Portfolio/src/components/about.vue
+++ Elias's Portfolio/src/components/about.vue
@@ -37,14 +37,14 @@
             </svg>
             <p>C language</p>
           </div>
-          <div class="C#">
+          <div class="Csharp">
             <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>C#</title>
               <path
                   d="M1.194 7.543v8.913c0 1.103.588 2.122 1.544 2.674l7.718 4.456a3.086 3.086 0 0 0 3.088 0l7.718-4.456a3.087 3.087 0 0 0 1.544-2.674V7.543a3.084 3.084 0 0 0-1.544-2.673L13.544.414a3.086 3.086 0 0 0-3.088 0L2.738 4.87a3.085 3.085 0 0 0-1.544 2.673Zm5.403 2.914v3.087a.77.77 0 0 0 .772.772.773.773 0 0 0 .772-.772.773.773 0 0 1 1.317-.546.775.775 0 0 1 .226.546 2.314 2.314 0 1 1-4.631 0v-3.087c0-.615.244-1.203.679-1.637a2.312 2.312 0 0 1 3.274 0c.434.434.678 1.023.678 1.637a.769.769 0 0 1-.226.545.767.767 0 0 1-1.091 0 .77.77 0 0 1-.226-.545.77.77 0 0 0-.772-.772.771.771 0 0 0-.772.772Zm12.35 3.087a.77.77 0 0 1-.772.772h-.772v.772a.773.773 0 0 1-1.544 0v-.772h-1.544v.772a.773.773 0 0 1-1.317.546.775.775 0 0 1-.226-.546v-.772H12a.771.771 0 1 1 0-1.544h.772v-1.543H12a.77.77 0 1 1 0-1.544h.772v-.772a.773.773 0 0 1 1.317-.546.775.775 0 0 1 .226.546v.772h1.544v-.772a.773.773 0 0 1 1.544 0v.772h.772a.772.772 0 0 1 0 1.544h-.772v1.543h.772a.776.776 0 0 1 .772.772Zm-3.088-2.315h-1.544v1.543h1.544v-1.543Z"/>
             </svg>
             <p>C#</p>
           </div>
-          <div class="HTML">
+          <div class="html">
             <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>HTML5</title>
               <path
                   d="M1.5 0h21l-1.91 21.563L11.977 24l-8.564-2.438L1.5 0zm7.031 9.75l-.232-2.718 10.059.003.23-2.622L5.412 4.41l.698 8.01h9.126l-.326 3.426-2.91.804-2.955-.81-.188-2.11H6.248l.33 4.171L12 19.351l5.379-1.443.744-8.157H8.531z"/>
@@ -143,6 +143,7 @@ export default {
 }
 
 .badges-container {
+  gap: 0 5px;
   background-color: transparent;
   display: flex;
   justify-content: center;
@@ -168,14 +169,43 @@ export default {
   align-items: center;
   width: fit-content;
   cursor: default;
-  transition: transform 0.3s ease-in-out, background-image 0.3s ease-in-out, color 0.3s ease-in-out;
+  transition: background-color 0.1s ease-in-out;
 }
 
-.badges-container div:hover {
-  background-image: linear-gradient(43deg, #4158D0, #C850C0, #FFCC70);
-  color: transparent;
-  -webkit-background-clip: text;
-  background-clip: text;
+.python:hover {
+  transform: scale(101%);
+  border-color: rgba(206, 196, 0, 0.76);
+  background-color: rgba(183, 178, 73, 0.24);
+}
+
+.C:hover, .Godot:hover{
+  transform: scale(101%);
+  border-color: rgba(0, 119, 152, 0.76);
+  background-color: rgba(73, 157, 183, 0.24);
+}
+
+.NodeJS:hover, .CSFML:hover {
+  transform: scale(101%);
+  border-color: rgba(43, 152, 0, 0.76);
+  background-color: rgba(113, 183, 73, 0.24);
+}
+
+.Csharp:hover, .Roblox_Studio:hover{
+  transform: scale(101%);
+  border-color: rgba(87, 0, 255, 0.76);
+  background-color: rgba(104, 73, 183, 0.24);
+}
+
+.Git:hover, .html:hover{
+  transform: scale(101%);
+  border-color: rgba(255, 117, 0, 0.76);
+  background-color: rgba(183, 135, 73, 0.24);
+}
+
+.Github:hover, .Unity:hover{
+  transform: scale(101%);
+  border-color: rgba(126, 126, 126, 0.76);
+  background-color: rgba(0, 0, 0, 0.24);
 }
 
 .badges div p {
--- Elias's Portfolio/src/components/content.vue
+++ Elias's Portfolio/src/components/content.vue
@@ -4,19 +4,26 @@
     <div class="project">
       <h1>Discord Bot</h1>
       <img class="bot-img" alt="Atlanta" src="@/assets/atlantaVR.png">
-      <h3 class="description-discord-bot">Discord bot created for a community server, he's connected to a Database to stock the events.
-        I've create a website to add, edit, delete & view all of events</h3>
+      <h3 class="description-discord-bot">Discord bot created for a community server, he's connected to a Database to stock the events.<br>I've create a website to add, edit, delete & view all of events<br><br></h3>
       <p class="languages">Python, NodeJs, MySQL, HTML, CSS</p>
     </div>
     <div class="project">
       <h1>Minecraft Server</h1>
+      <img class="ptero-img" alt="MC" src="@/assets/ptero.png"/>
+      <h3 class="description-discord-bot">I've created a Minecraft server and managed the synchronization of all servers.<br>We were two developers on this project. It started in 2020 and ended in 2022</h3>
       <p class="languages">Panel Pterodactyl, Ubuntu Server, Java, HTML, CSS, SpigotMC</p>
     </div>
     <div class="project">
-      <h1>sokoban</h1>
+      <h1>Sokoban</h1>
+      <img class="sokoban-image" src="@/assets/sokoban.png" alt="sokoban-img">
+      <h3 class="description-discord-bot">As part of a project at Epitech, we recreated the famous game Sokoban in the Linux terminal using NCurses.<br><br><br></h3>
+      <p class="languages">C, NCurses, UNIX</p>
     </div>
     <div class="project">
-      <h1>my_navy</h1>
+      <h1>My_hunter</h1>
+      <img class="hunter-image" src="@/assets/hunter.png" alt="hunter-img">
+      <h3 class="description-discord-bot">As part of a project at Epitech, we recreated the famous game Duck Hunt in C using a graphical & audio library</h3>
+      <p class="languages">C, CSFML</p>
     </div>
   </div>
 </template>
@@ -30,6 +37,18 @@ export default {
 <style>
 .description-discord-bot {
   max-width: 90%;
+}
+
+.ptero-img {
+  max-width: 90%;
+}
+
+.sokoban-image {
+  max-width: 90%;
+}
+
+.hunter-image {
+  max-width: 60%;
 }
 
 .languages {
@@ -74,7 +93,7 @@ export default {
 }
 
 .bot-img {
-  width: 240px;
+  max-width: 90%;
 }
 
 #Cards div h1 {
@@ -88,6 +107,7 @@ export default {
 }
 
 #Cards {
+  gap: 20px;
   justify-content: center;
   border-radius: 8px;
   position: relative;
