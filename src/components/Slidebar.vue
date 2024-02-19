8f38b8859190a877ff0ed0b1f5f0cb59d1fb7469
Elias Josué HAJJAR LLAUQUEN <elias-josue.hajjar-llauquen@epitech.eu>
2024-02-19 11:11:33 +0100

    [TEST] auto deploy test

--- src/components/Slidebar.vue
+++ src/components/Slidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="zone">
     <div class="hello">
-      <button class="clickable1" @click="scrollToAbout">About.</button>
+      <button class="clickable1" @click="scrollToAbout">keke</button>
       <button class="clickable2" @click="scrollToProject">Projects</button>
       <button class="clickable3" @click="scrollToContact">Contact</button>
     </div>
@@ -20,7 +20,7 @@ export default {
     },
     scrollToContact() {
       window.scrollTo({
-        top: 5000,
+        top: 4000,
         behavior: 'smooth'
       });
     },
